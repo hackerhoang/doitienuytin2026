9f4aaa029fed513b1a2b352b21c4290d91dff7e7
doitienuytin <hoanggaming1309@gmail.com>
2025-12-10 08:21:08 +0700

    Add files via upload

---
+++ deepseek_bash_20251210_aa22f0.sh
@@ -0,0 +1,10 @@
+# Tạo repository mới trên GitHub
+# Clone về máy
+git clone https://github.com/username/locphat-exchange.git
+cd locphat-exchange
+
+# Copy file index.html vào
+# Thêm các file cần thiết
+git add .
+git commit -m "Initial commit with Firebase integration"
+git push origin main
